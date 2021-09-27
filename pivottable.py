da1726715a5fdb54da484fc811ed9aab2b3e79c2
Nguyen Dac Doan <nguyendacdoan@gmail.com>
2021-09-27 11:37:55 +0700

    UI

--- pivottable.py
+++ pivottable.py
@@ -3,15 +3,15 @@ from urllib import request
 import json, io
 
 def UI(df, outfile_path = "pivottableUI.html", **kwargs):
-  try: pivottable.TEMPLATE
+  try: UI.TEMPLATE
   except:
     with request.urlopen('https://aiaicafe.github.io/react-pivottable-template/') as url:
-      pivottable.TEMPLATE = url.read().decode()
+      UI.TEMPLATE = url.read().decode()
 
   with io.open(outfile_path, 'wt', encoding='utf8') as outfile:
     csv = df.to_csv(encoding='utf8')
     if hasattr(csv, 'decode'):
       csv = csv.decode('utf8')
-    outfile.write(pivottable.TEMPLATE %
+    outfile.write(UI.TEMPLATE %
       dict(csv=csv, kwargs=json.dumps(kwargs)))
   return HTML(outfile_path)
